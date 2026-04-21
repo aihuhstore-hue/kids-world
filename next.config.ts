c75c58354ec79626e7dd86d262d70a0951b59026
dk <aihuhstore@gmail.com>
2026-04-21 23:03:54 +0100

    allow cloudinary images

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "picsum.photos" },
       { protocol: "https", hostname: "cdn.dummyjson.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
     ],
   },
 };
